e52d001d2207d1795d6f3dc687083b831a432de9
Manish Kumar Chintakayala <106020014+ChManish2003@users.noreply.github.com>
2025-06-26 16:14:56 +0530

    Index.tsx

---
+++ src/pages/Index.tsx
@@ -0,0 +1,12 @@
+
+import ChatAssistant from "@/components/ChatAssistant";
+
+const Index = () => {
+  return (
+    <div className="h-screen bg-gray-900">
+      <ChatAssistant />
+    </div>
+  );
+};
+
+export default Index;
